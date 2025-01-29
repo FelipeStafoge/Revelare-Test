0c8c19c68be46fb7547ca175fb1ced15a6c3a0a7
FelipeStafoge <felipestafoge11@gmail.com>
2025-01-29 09:44:54 -0300

    Add verceljson

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import Blog from "./modules/Blog";
-import { BrowserRouter, Route, Routes } from "react-router";
 import Home from "./modules/Home";
+import { BrowserRouter, Route, Routes } from "react-router";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route key={"HomeKey"} path="/" element={<Home />} />
-      </Routes>
-      <Routes>
         <Route key={"BlogKey"} path="/blog" element={<Blog />} />
       </Routes>
     </BrowserRouter>
